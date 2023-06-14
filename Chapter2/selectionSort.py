39b7c1648a084498b2b4da4673e8b45f9f47e9da
pairofAces <karanchauhan16@gmail.com>
2023-06-13 21:30:39 -0400

    created function to find smallest element in an array

--- Chapter2/selectionSort.py
+++ Chapter2/selectionSort.py
@@ -1,1 +1,10 @@
 # create an initial function that will find the smallest element in an array
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
