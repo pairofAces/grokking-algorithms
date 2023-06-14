c080a7b06c9ee142998e6343a2324e777cc3a8a4
pairofAces <karanchauhan16@gmail.com>
2023-06-13 21:34:08 -0400

    created solution for selection sort

--- Chapter2/selectionSort.py
+++ Chapter2/selectionSort.py
@@ -8,3 +8,17 @@ def findSmallest(arr):
             smallest = arr[i]
             smallest_index = i
     return smallest_index
+
+def selectionSort(arr):
+    # create a new array
+    newArr = []
+
+    # traverse the array
+    for i in range(len(arr)):
+        # invoke the findSmallest function to find the smallest element's index
+        # and append that element into the new array
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+
+    # return the new array
+    return newArr
